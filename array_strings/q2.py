8444cda724447dd42e268dfc4f7e60f5a71908a6
[Eric Chou] <[ericchoujr777@gmail.com]>
2021-07-10 23:55:20 -0700

    Changes q2.py

---
+++ array_strings/q2.py
@@ -0,0 +1,48 @@
+
+
+
+"""
+Approach:
+
+
+Time Complexity:
+Space Complexity:
+"""
+
+
+def isPermutation(str1, str2):
+
+
+    if len(str1) != len(str2):
+        return False
+
+
+    countDict1 = {}
+    countDict2 = {}
+
+
+    for i in str1:
+        if i in countDict1:
+            countDict1[i] += 1
+        else:
+            countDict1[i] =1
+
+    for j in str2:
+        if j in countDict2:
+            countDict2[j] += 1
+        else:
+            countDict2[j] =1
+    
+    for i in str1:
+        if countDict1[i] != countDict2[i]:
+            return False
+
+    return True
+    
+
+
+
+if __name__ == "__main__":
+
+
+    print(isPermutation("afefef", "afefea"))
